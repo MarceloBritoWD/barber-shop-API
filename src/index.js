7420b5abf9fb4c5a06b8dbfdca3468d1c34a79a2
Marcelo Brito <marcelobrito@Marcelos-MacBook-Air.local>
2019-03-22 09:11:17 -0300

    #master - Insering morgan for http logs and env-port support.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+const PORT = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+let logStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), {flags: 'a'})
+app.use(morgan('combined', { stream: logStream }));
 
 app.use('/auth', require('./controller/auth.controller'))
 app.use('/barber-shop', require('./controller/barber-shop.controller'))
 
 
-app.listen(3000, () => {
-    console.log('Application running at http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Application running at http://localhost:${PORT}`);
 })
